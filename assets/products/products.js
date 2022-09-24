aa89ac24e4dce937d389529a5add3f5ca05a4705
Artus <artuslama@gmail.com>
2022-09-24 17:11:37 +0200

    -

---
+++ assets/products/products.js
@@ -0,0 +1,24 @@
+
+
+var products = 
+{
+
+    "bottle-1": {
+        name: "Green Bottle",
+        price: 19.99
+    }, 
+    "bottle-2": {
+        name: "Pink Bottle",
+        price: 19.99
+    }, 
+    "bottle-3": {
+        name: "White Bottle",
+        price: 19.99
+    }, 
+    "bottle-4": {
+        name: "Gray Bottle",
+        price: 19.99
+    }
+
+}
+
--- buypage.js
+++ buypage.js
@@ -1,15 +1,25 @@
 
+
 function getCartItems() {
     var items = JSON.parse(localStorage.getItem("cartItems"));
     if (items == null) return [];
     return items;
 }
 
+
 window.onload = displayProductList;
 function displayProductList() {
     console.log(getCartItems());
     for (var item of getCartItems()){
-        document.getElementById("productList").innerHTML += "<li><img src='assets/products/" + item + ".png'></li>";
+        var product = products[item];
+        if (product == null){
+            var name = "PRODUCT NOT FOUND";
+            var price = "";
+        } else {
+            var name = product.name;
+            var price = product.price;
+        }    
+        document.getElementById("productList").innerHTML += "<li><img src='assets/products/" + item + ".png'>" + name + " price: " + price + "</li>";
     }
 }
 
