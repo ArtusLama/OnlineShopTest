47b0b51ed104c6a463b1f6d872d7de116ead97fc
Artus <artuslama@gmail.com>
2022-12-15 20:16:46 +0100

    add new products

--- assets/products/products.js
+++ assets/products/products.js
@@ -5,24 +5,62 @@ var products =
 
     "bottle-1": {
         name: "Green Bottle",
-        price: 19.99
+        price: 19.99,
+        categories: [
+            "category_bottle",
+            "color_green"
+        ]
     }, 
     "bottle-2": {
         name: "Pink Bottle",
-        price: 19.99
+        price: 19.99,
+        categories: [
+            "category_bottle",
+            "color_pink"
+        ]
     }, 
     "bottle-3": {
         name: "White Bottle",
-        price: 19.99
+        price: 19.99,
+        categories: [
+           "category_bottle",
+           "color_white"
+        ]
     }, 
     "bottle-4": {
         name: "Gray Bottle",
-        price: 19.99
+        price: 19.99,
+        categories: [
+            "category_bottle",
+            "color_black"
+        ]
     },
 
     "light_blue_bag": {
         name: "Light blue bag",
-        price: 30
+        price: 30,
+        categories: [
+            "category_bag",
+            "color_blue"
+        ]
+    },
+    
+    "unicorn_scooter": {
+        name: "Einhorn Roller",
+        price: 149.99,
+        categories: [
+            "category_movement",
+            "color_pink"
+        ]
+    },
+    
+    "TaubenHut": {
+        name: "Tauben Hut",
+        price: 34.99,
+        categories: [
+            "category_hat",
+            "color_blue"
+        ]
     }
 
 }
